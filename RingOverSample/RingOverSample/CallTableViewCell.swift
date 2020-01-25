8b7fd913b275b37db8772fbe23b130b32734c98d
Rachid <rach.benhamou@gmail.com>
2020-01-25 15:46:26 +0100

    recent cell

---
+++ RingOverSample/RingOverSample/CallTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CallTableViewCell.swift
+//  RingOverSample
+//
+//  Created by MacBook on 25/01/2020.
+//  Copyright © 2020 ringover. All rights reserved.
+//
+
+import UIKit
+
+class CallTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- RingOverSample/RingOverSample/ViewController.swift
+++ RingOverSample/RingOverSample/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CallCell", for: indexPath)
+        
+        cell.textLabel?.text = "headlines[indexPath.row].title"
+        
+        return cell
+    }
 
 }
 
