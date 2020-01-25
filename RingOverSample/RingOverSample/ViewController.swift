6b8be03d2064d83c61d58212a14e7bc05e7cf47d
Rachid <rach.benhamou@gmail.com>
2020-01-25 13:57:30 +0100

    start project

---
+++ RingOverSample/RingOverSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RingOverSample
+//
+//  Created by MacBook on 25/01/2020.
+//  Copyright © 2020 ringover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
